8ec2e3b7e2af8845c788de7f14300e3b52edfb34
SONGI <ahnsongi6769@gmail.com>
2026-01-29 09:12:56 +0900

    JSTL 주요 기능 사용법

--- SpringStudy/src/main/java/com/app/dto/study/Product.java
+++ SpringStudy/src/main/java/com/app/dto/study/Product.java
@@ -1,12 +1,19 @@
 package com.app.dto.study;
 
 public class Product {
-	
+
 	public String id;
 	public String name;
 	public int price;
 	
+	public Product() {}
 	
+	public Product(String id, String name, int price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
 	public String getId() {
 		return id;
 	}
@@ -29,9 +36,10 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
 	}
-		
 	
 	
-
-
 }
+
+
+
+
