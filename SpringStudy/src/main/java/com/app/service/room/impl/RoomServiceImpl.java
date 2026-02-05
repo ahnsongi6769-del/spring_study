b53509096b1a66b8c6e97fc6da74ac4fc6af63e0
SONGI <ahnsongi6769@gmail.com>
2026-02-05 11:51:04 +0900

    관리자페이지 객실 정보 삭제 처리

--- SpringStudy/src/main/java/com/app/service/room/impl/RoomServiceImpl.java
+++ SpringStudy/src/main/java/com/app/service/room/impl/RoomServiceImpl.java
@@ -55,4 +55,13 @@ public class RoomServiceImpl implements RoomService {
 		return room;
 	}
 
+	@Override
+	public int removeRoom(int roomId) {
+
+		int result = roomDAO.removeRoom(roomId);
+		
+		return result;
+	}
+
 }
+
